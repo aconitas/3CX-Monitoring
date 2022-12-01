3046edf8e946eefe5b31685d3cbb64024a2b4678
danido95 <103109003+danido95@users.noreply.github.com>
2022-12-01 16:20:50 +0100

    adds '3CX Console Restriction aktiv'

---
+++ 3CX/Console Restriction aktiv/3CX Console Restriction aktiv.sh
@@ -0,0 +1,27 @@
+#   Herausgeber: aconitas® GmbH - Bäumenheimer Str. 5 - 86690 Mertingen
+#   Website: https://www.aconitas.com
+#   Telefon: +49 (906) 126725-0
+#   E-Mail: info@aconitas.com
+#
+#   Version 1.0
+
+
+
+########################################################
+#### Import WebServerPort in Variable
+HTTP_PORT=$(cat /tmp/3cx.port)
+#echo $HTTP_PORT
+
+########################################################
+#### Abruf Werte aus 3CX Web Console
+RESULT=$(curl --request GET --cookie /tmp/3cxcookie --cookie-jar /tmp/3cxcookie -s localhost:${HTTP_PORT}/api/IpBlackList)
+
+########################################################
+#### Auswertung
+if [[ $RESULT =~ 'Console Restriction added from Management Console' ]]; then
+  echo Console Restriction aktiv
+  exit 0
+else
+  echo Console Restriction NICHT aktiv
+  exit 2
+fi

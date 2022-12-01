bef25b46ba413e6bdce63bc905b1d2ea49e072f7
danido95 <103109003+danido95@users.noreply.github.com>
2022-12-01 16:21:11 +0100

    adds '3CX geplantes Backup aktiv'

---
+++ 3CX/Geplantes Backup aktiv/3CX geplantes Backup aktiv.sh
@@ -0,0 +1,30 @@
+#   Herausgeber: aconitas® GmbH - Bäumenheimer Str. 5 - 86690 Mertingen
+#   Website: https://www.aconitas.com
+#   Telefon: +49 (906) 126725-0
+#   E-Mail: info@aconitas.com
+#
+#   Version 1.0
+
+
+
+########################################################
+#### Import WebServerPort in Variable
+HTTP_PORT=$(cat /tmp/3cx.port)
+#echo $HTTP_PORT
+
+########################################################
+#### Abruf Werte aus 3CX Web Console
+RESULT=$(curl --request GET --cookie /tmp/3cxcookie --cookie-jar /tmp/3cxcookie -s localhost:${HTTP_PORT}/api/SystemStatus)
+
+########################################################
+#### Auswertung
+if [[ $RESULT =~ '"BackupScheduled":true' ]]; then
+  echo Backup Job aktiv
+  exit 0
+elif [[ $RESULT =~ '"BackupScheduled":false' ]]; then
+  echo Backup Job NICHT aktiv
+  exit 2
+else
+  echo Es liegt ein Fehler in der Konfiguration des Checks vor oder der Server ist nicht erreichbar
+  exit 1
+fi

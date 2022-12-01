0ab5fa0d042c3b280398a6c0722fdeca886c2776
danido95 <103109003+danido95@users.noreply.github.com>
2022-12-01 16:22:06 +0100

    adds '3CX Prüfung unsichere Zugangsdaten'

---
+++ 3CX/Prüfung unsichere Zugangsdaten/3CX Prüfe unsichere Zugangsdaten.sh
@@ -0,0 +1,27 @@
+#   Herausgeber: aconitas® GmbH - Bäumenheimer Str. 5 - 86690 Mertingen
+#   Website: https://www.aconitas.com
+#   Telefon: +49 (906) 126725-0
+#   E-Mail: info@aconitas.com
+#
+#   Version 1.0
+
+
+
+########################################################
+#### Import WebServerPort in Variable
+HTTP_PORT=$(cat /tmp/3cx.port)
+#echo $HTTP_PORT
+
+########################################################
+#### Abruf Werte aus 3CX Web Console
+RESULT=$(curl --request GET --cookie /tmp/3cxcookie --cookie-jar /tmp/3cxcookie -s localhost:${HTTP_PORT}/api/ExtensionList)
+
+########################################################
+#### Auswertung
+if [[ $RESULT =~ '"invalidPasswords":[0' || $RESULT =~ '"invalidPasswords":[1' || $RESULT =~ '"invalidPasswords":[2' ]]; then
+  echo Eine oder mehrere Nebenstellen nutzen UNSICHERE Zugangsdaten
+ exit 2
+else
+  echo Alle Nebenstellen nutzen sichere Zugangsdaten
+  exit 0
+fi

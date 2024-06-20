7c25d38d99be8b3b6e5b58b618ad4571b693dc00
kp9209 <kevin.pascher@aconitas.com>
2024-06-20 08:31:57 +0200

    Add files via upload

---
+++ Scripts (use with local Linux Monitoring Agent)/v18/002 - Anzahl Nebenstellen/3CX Anzahl Nebenstellen.sh
@@ -0,0 +1,20 @@
+##########################################################################
+#
+#   aconitas® GmbH · Bäumenheimer Str. 5 · 86690 Mertingen · Germany
+#   https://www.aconitas.com · info@aconitas.com · +49 (906) 126725-0
+#
+#   Version 1.0 · 2022-12-15
+#
+##########################################################################
+
+########################################################
+#### Import WebServerPort in Variable
+HTTP_PORT=$(cat /tmp/3cx.port)
+
+########################################################
+#### Abruf Werte aus 3CX Web Console
+RESULT=$(curl --request GET --cookie /tmp/3cxcookie --cookie-jar /tmp/3cxcookie -s localhost:${HTTP_PORT}/api/SystemStatus  | egrep -io '"ExtensionsTotal":[^,]*' | cut -d ':'  -f 2)
+
+########################################################
+#### Auswertung
+echo $RESULT Nebenstellen sind im System konfiguriert
